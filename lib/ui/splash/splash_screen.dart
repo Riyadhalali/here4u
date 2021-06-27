c16abd8817af17369a80d081f74312f8b41ead37
Riyad <eng.riyadalali93@gmail.com>
2021-06-27 23:01:18 +0300

    Created Home page and added timer to navigate to the next screen when timer is done loading or the time is finished

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,7 +1,31 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:here4u/ui/home/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
   static const String id = 'splash_screen';
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  Future<Timer> LoadTimer() async {
+    return new Timer(Duration(seconds: 2), onDoneLoading);
+  }
+
+  Future onDoneLoading() async {
+    Navigator.pushNamed(context, HomeScreen.id);
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    LoadTimer();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
